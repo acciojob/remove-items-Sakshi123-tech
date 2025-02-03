c8604186f03b07084abb9194c005654cffe6cf36
Sakshi123-tech <sakshiagnihotri292@gmail.com>
2025-02-03 22:27:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-let dropdown = document.getElementById('colorSelect');
-let button = document.getElementById('removeBtn');
+// let dropdown = document.getElementById('colorSelect');
+// let button = document.getElementById('removeBtn');
 
-console.log('Dropdown:', dropdown); // This should log the dropdown element
-console.log('Button:', button); // This should log the button element
+// console.log('Dropdown:', dropdown); // This should log the dropdown element
+// console.log('Button:', button); // This should log the button element
 
-// Add event listener to the button
-button.addEventListener('click', function() {
-    // Get the selected option
-    let selectedOption = dropdown.options[dropdown.selectedIndex];
+// // Add event listener to the button
+// button.addEventListener('click', function() {
+//     // Get the selected option
+//     let selectedOption = dropdown.options[dropdown.selectedIndex];
 
-    console.log('Selected option:', selectedOption); // This should log the selected option
+//     console.log('Selected option:', selectedOption); // This should log the selected option
 
-    // Remove the selected option
-    dropdown.remove(selectedOption.index);
-});
+//     // Remove the selected option
+//     dropdown.remove(selectedOption.index);
+// });
+window.onload = function() {
+    let dropdown = document.getElementById('colorSelect');
+    let button = document.getElementById('removeBtn');
+
+    button.addEventListener('click', function() {
+        dropdown.remove(dropdown.selectedIndex);
+    });
+}
