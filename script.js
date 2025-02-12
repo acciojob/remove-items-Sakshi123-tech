21f07289988b6631a94ea387d4bf3dcd8a2e209b
Sakshi123-tech <sakshiagnihotri292@gmail.com>
2025-02-12 23:55:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
-// let dropdown = document.getElementById('colorSelect');
-// let button = document.getElementById('removeBtn');
+document.addEventListener('DOMContentLoaded', function() {
+    var removeButton = document.getElementById('removeButton');
+    var colorSelect = document.getElementById('colorSelect');
 
-// console.log('Dropdown:', dropdown); // This should log the dropdown element
-// console.log('Button:', button); // This should log the button element
-
-// // Add event listener to the button
-// button.addEventListener('click', function() {
-//     // Get the selected option
-//     let selectedOption = dropdown.options[dropdown.selectedIndex];
-
-//     console.log('Selected option:', selectedOption); // This should log the selected option
-
-//     // Remove the selected option
-//     dropdown.remove(selectedOption.index);
-// });
-window.onload = function() {
-    let dropdown = document.getElementById('colorSelect');
-    let button = document.getElementById('removeBtn');
-
-    button.addEventListener('click', function() {
-        dropdown.remove(dropdown.selectedIndex);
-    });
-}
+    if (removeButton && colorSelect) {
+        removeButton.addEventListener('click', function() {
+            colorSelect.remove(colorSelect.selectedIndex);
+        });
+    }
+});
